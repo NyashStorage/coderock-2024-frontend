9ee18095bf19cc25d32710a10795e9e42b999c43
Daniil Koshkin <admin@nyashmyash99.ru>
2024-04-08 21:34:39 +0300

    Улучшены стили компонента Avatar и их формирование

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,19 +1,23 @@
-import type { JSX, PropsWithChildren } from 'react';
-
 import '../assets/styles/components/avatar/index.scss';
+import type { JSX, PropsWithChildren } from 'react';
 
 export interface AvatarProps extends PropsWithChildren {
   image?: string;
   rounded?: boolean;
+  onClick?: () => void;
 }
 
 export default function Avatar({
-  rounded = false,
   image,
+  rounded = false,
+  onClick,
 }: AvatarProps): JSX.Element {
-  return (
-    <div className={rounded ? 'avatar--rounded' : 'avatar'}>
-      <img src={image} onError={(e) => (e.target.style.display = 'none')} />
-    </div>
-  );
+  function getStyles(): string {
+    const styles = ['avatar'];
+    if (rounded) styles.push('avatar--rounded');
+    if (onClick) styles.push('avatar--hoverable');
+    return styles.join(' ');
+  }
+
+  return <img className={getStyles()} src={image} alt="" onClick={onClick} />;
 }
