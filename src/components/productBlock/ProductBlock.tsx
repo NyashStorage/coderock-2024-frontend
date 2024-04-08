33a1e1fe648089127f1c5c4af9e3b239e7be4902
Noga-Ekaterina <katrin210607@gmail.com>
2024-04-08 22:30:46 +0300

    Стилизовала компонент ProductBlock.

--- src/components/productBlock/ProductBlock.tsx
+++ src/components/productBlock/ProductBlock.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { JSX, PropsWithChildren } from 'react';
+import '../../assets/styles/components/productBlock/index.scss';
+import RatingButton from '../buttons/RatingButton';
 
-function ProductBlock(props) {
+export interface ProductBlockProps extends PropsWithChildren {
+  img: string;
+  name: string;
+  company: string;
+  price: number;
+  rating: number;
+  numberOfReviews: number;
+}
+function ProductBlock({
+  img,
+  name,
+  company,
+  price,
+  rating,
+  numberOfReviews,
+}: ProductBlockProps): JSX.Element {
   return (
-    <div>
-      <img src="" alt=""/>
+    <div className="productBlock w-72 bg-white shadow-gray-400 shadow-sm rounded">
+      <img src={img} alt="" className="w-full" />
+      <div className="p-5">
+        <div className="productBlock__name font-bold text-xl">{name}</div>
+        <div className="productBlock__company">{company}</div>
+        <div className="productBlock__price">{price}&#8381;</div>
+        <hr className="my-4" />
+        <div className="flex justify-between items-center">
+          <RatingButton defaultRating={rating} disabled={true} />
+          <div className="text-sm productBlock__numberOfReviews">
+            <span className="font-bold">{numberOfReviews} </span>
+            отзывов
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/stories/ProductBlock.stories.tsx
+++ src/stories/ProductBlock.stories.tsx
@@ -11,3 +11,11 @@ const Template: StoryFn<typeof ProductBlock> = (args) => (
 );
 
 export const Default = Template.bind({});
+Default.args = {
+  img: 'https://i.pinimg.com/736x/90/bf/6b/90bf6be4825e15296032e93a0d0736eb.jpg',
+  name: 'Супер автомат',
+  company: 'weapons of the future',
+  price: 10000,
+  rating: 3,
+  numberOfReviews: 65,
+};
