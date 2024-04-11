aac621113a0cfb9c844ead797b19007ddc5b0309
Daniil Koshkin <admin@nyashmyash99.ru>
2024-04-11 19:28:24 +0300

    Добавлена зависимость "react-router-dom"

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,18 @@
-import type { Preview } from '@storybook/react';
+import { JSX } from 'react';
+import type { Preview, StoryContext } from '@storybook/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-export default {} as Preview;
+export const withRouter = (Story: JSX.ElementType, _: StoryContext) => {
+  return (
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: '*',
+          element: <Story />,
+        },
+      ])}
+    />
+  );
+};
+
+export const parameters: Preview = {};
